0798549db14b19ac816ec1fffffc3020e55e05f1
Daniel Ray Ward <drward3@uno.edu>
2016-03-02 17:59:58 -0600

    added extension file

---
+++ Class_Examples/Lab 6/Lab 6/Extensions.swift
@@ -0,0 +1,57 @@
+//
+//  Extensions.swift
+//  lab4
+//
+//  Created by Daniel Ward on 2/17/16.
+//  Copyright © 2016 Daniel Ward. All rights reserved.
+//
+
+
+// Copied from https://gist.github.com/yannickl/16f0ed38f0698d9a8ae7 and http://classictutorials.com/2014/08/generate-a-random-color-in-swift/
+import Foundation
+import UIKit
+
+extension UIColor {
+    convenience init(hexString:String) {
+        let hexString:NSString = hexString.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+        let scanner            = NSScanner(string: hexString as String)
+        
+        if (hexString.hasPrefix("#")) {
+            scanner.scanLocation = 1
+        }
+        
+        var color:UInt32 = 0
+        scanner.scanHexInt(&color)
+        
+        let mask = 0x000000FF
+        let r = Int(color >> 16) & mask
+        let g = Int(color >> 8) & mask
+        let b = Int(color) & mask
+        
+        let red   = CGFloat(r) / 255.0
+        let green = CGFloat(g) / 255.0
+        let blue  = CGFloat(b) / 255.0
+        
+        self.init(red:red, green:green, blue:blue, alpha:1)
+    }
+    
+    func toHexString() -> String {
+        var r:CGFloat = 0
+        var g:CGFloat = 0
+        var b:CGFloat = 0
+        var a:CGFloat = 0
+        
+        getRed(&r, green: &g, blue: &b, alpha: &a)
+        
+        let rgb:Int = (Int)(r*255)<<16 | (Int)(g*255)<<8 | (Int)(b*255)<<0
+        
+        return NSString(format:"#%06x", rgb) as String
+    }
+    
+    class func random() -> UIColor{
+        let randomRed:CGFloat = CGFloat(drand48())
+        let randomGreen:CGFloat = CGFloat(drand48())
+        let randomBlue:CGFloat = CGFloat(drand48())
+        return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
+    }
+}
