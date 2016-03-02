9e39a9b028d5ac0107bb7a8ecdf8f95fdcb04ab4
Daniel Ray Ward <drward3@uno.edu>
2016-03-02 16:42:04 -0600

    added lab 5 code

---
+++ Class_Examples/lab5/lab4/CustomCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCell.swift
+//  lab4
+//
+//  Created by Daniel Ward on 2/22/16.
+//  Copyright © 2016 Daniel Ward. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+    @IBOutlet weak var customView: UIView!
+    @IBOutlet weak var customTextLabel: UILabel!
+    
+}
---
+++ Class_Examples/lab5/lab4/DetailViewController.swift
@@ -0,0 +1,46 @@
+//
+//  DetailViewController.swift
+//  lab4
+//
+//  Created by Daniel Ward on 2/17/16.
+//  Copyright © 2016 Daniel Ward. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    var detailItem: AnyObject? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = self.detailItem as? UIColor {
+            if let label = self.detailDescriptionLabel {
+                label.text = detail.toHexString()
+                self.view.backgroundColor = detail
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
