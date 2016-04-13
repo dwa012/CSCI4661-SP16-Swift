74c72e2f063abede9938148f32344ec94ed3137a
Daniel Ray Ward <drward3@uno.edu>
2016-04-13 17:57:45 -0500

    base for lab 10

---
+++ Class_Examples/Lab10/Lab10/ContactFormViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ContactFormViewController.swift
+//  Lab10
+//
+//  Created by Daniel Ward on 4/13/16.
+//  Copyright © 2016 CSCI4669. All rights reserved.
+//
+
+import UIKit
+import Eureka
+
+class ContactFormViewController: FormViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 3: Add save contact logic here
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func saveContact(sender: AnyObject) {
+        // 4: Add save contact logic here
+    }
+}
